b75a35af4d913a6dd7df312be1d295c02b228d8b
Michael Minchin <mike.minchin@btinternet.com>
2019-10-19 22:05:14 +0100

    Added tests project and started first unit test

---
+++ Interview/Interview.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Interview.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Interview.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e67540e3-860b-4913-9845-0342b063dd30")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Interview/Interview.Tests/Repository_Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Interview.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void StringRepository_SaveStorable_SetsCountToOne()
+        {
+            // Arrange
+            Repository<IStorable<string>, string> stringRepository = new Repository<IStorable<string>, string>();
+
+
+            // Act
+
+            // Assert
+
+        }
+    }
+}
