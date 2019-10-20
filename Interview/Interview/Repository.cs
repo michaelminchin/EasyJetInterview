5be6bd5ea793a72df66f4f08a14060f1a5863b24
Michael Minchin <mike.minchin@btinternet.com>
2019-10-20 11:23:03 +0100

    Added logger and logging calls to repository

--- Interview/Interview/Repository.cs
+++ Interview/Interview/Repository.cs
@@ -1,21 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Interview;
+
 
 namespace Interview
 {
     public class Repository<T, I> : IRepository<T, I> where T : IStoreable<I>
     {
         private ICollection<T> items;
+        private ILogger logger;
 
-        public Repository(ICollection<T> items)
+        public Repository(ICollection<T> items, ILogger logger)
         {
             if (items == null) throw new ArgumentNullException("Items collection cannot be null");
+            if (logger == null) throw new ArgumentNullException("Logger cannot be null");
 
             this.items = items;
+            this.logger = logger;
         }
 
         public void Delete(I id)
@@ -38,11 +38,15 @@ namespace Interview
             try
             {
                 items.Add(item);
+                logger.Log(LogLevel.Error, $"Item {item.Id} added to repository");
+            }
+            catch (ArgumentNullException ane)
+            {
+                logger.Log(LogLevel.Error, "Item cannot be null when calling Save on repository", ane);
             }
             catch (Exception e)
             {
-
-                throw e;
+                logger.Log(LogLevel.Error, "Error calling Save on repository", e);
             }
         }
     }
