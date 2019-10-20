c50e3fbc3284f60e06347eacbc45ba9b7ca68f7b
Michael Minchin <mike.minchin@btinternet.com>
2019-10-20 12:15:28 +0100

    Added test for Get, failing

--- Interview/Interview.Tests/Repository_Tests.cs
+++ Interview/Interview.Tests/Repository_Tests.cs
@@ -25,5 +25,21 @@ namespace Interview.Tests
             // Assert
             Assert.That(storeableCollection.Count == 1);
         }
+
+        [Test]
+        public void StringRepository_GetStorable_ReturnsStoreable()
+        {
+            // Arrange
+            ILogger logger = new Logger();
+            Storeable<string> storeable = new Storeable<string> { Id = "first" };
+            ICollection<IStoreable<string>> storeableCollection = new Collection<IStoreable<string>>();
+            Repository<IStoreable<string>, string> stringRepository = new Repository<IStoreable<string>, string>(storeableCollection, logger);
+
+            // Act
+            var returnedObject = stringRepository.Get(storeable.Id);
+
+            // Assert
+            Assert.IsTrue(returnedObject.GetType() == typeof(IStoreable<string>));
+        }
     }
 }
